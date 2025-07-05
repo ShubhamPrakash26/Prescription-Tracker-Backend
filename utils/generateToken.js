d341ac29c4c56cc0959471529f5baaac90cfdf8b
Shubham Prakash <prakashshubham26@gmail.com>
2025-07-05 15:57:52 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,15 +1,16 @@
 import jwt from "jsonwebtoken";
 
-export const createToken=(userId,res) =>{
-const token = jwt.sign({userId},process.env.JWT_SECRET,{
-    expiresIn:"45d",
-});
+export const createToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "45d",
+  });
 
-res.cookie("jwt",token,{
-    maxAge:45 * 24 * 60 * 60 * 1000,
-    httpOnly:true,
-    sameSite:"strict",
-    secure:process.env.NODE_ENV !=="development",
-});
-return token;
+  const isProduction = process.env.NODE_ENV === "production";
+  res.cookie("jwt", token, {
+    maxAge: 45 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "strict",
+    secure: isProduction,
+  });
+  return token;
 };
