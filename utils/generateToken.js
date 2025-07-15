46353bc5954e00af9fa46d04198ef0a0ba9832dc
Shubham Prakash <prakashshubham26@gmail.com>
2025-07-15 23:15:08 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,8 +8,8 @@ const token = jwt.sign({userId},process.env.JWT_SECRET,{
 res.cookie("jwt",token,{
     maxAge:45 * 24 * 60 * 60 * 1000,
     httpOnly:true,
-    sameSite:"strict",
-    secure:process.env.NODE_ENV !=="development",
+    secure: true,
+    sameSite: "strict"
 });
 return token;
 };
